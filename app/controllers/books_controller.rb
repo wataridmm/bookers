c0a4096bc6078fb33fc6f4080f5ba4eecc79eaad
wataridmm <r.watari.watari@gmail.com>
2021-09-11 07:36:20 +0000

    fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,23 @@ class BooksController < ApplicationController
   end
 
   def new
+    # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
+
