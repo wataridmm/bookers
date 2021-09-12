887ee081aca3c3a479cc39c1c20813cff5f3b977
wataridmm <r.watari.watari@gmail.com>
2021-09-12 17:29:33 +0000

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def index
     @books = Book.all
   end
@@ -19,6 +20,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to '/books'
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
