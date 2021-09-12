5c9123a9181439f3c415338053fd8240e641f753
wataridmm <r.watari.watari@gmail.com>
2021-09-12 18:00:04 +0000

    add middle

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,15 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    
+    if  @book.save
+    flash[:create] = 'Book was successfully created.'
+    redirect_to book_path(@book.id)
+    
+    end
+    
+    
   end
 
   def edit
@@ -24,9 +30,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to '/books'
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:update] = 'Book was successfully updated.'
+      
+    redirect_to book_path(@book.id)
+    end
   end
   
   def destroy
